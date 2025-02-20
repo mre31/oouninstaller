6ed8f0c3ae095d6fdf5c732e2118e55f59c53c58
NovaWeb <74352257+mre31@users.noreply.github.com>
2025-02-20 13:56:52 +0300

    Revert back latest comit

--- uninstall_oo.ps1
+++ uninstall_oo.ps1
@@ -92,52 +92,8 @@ try {
     @(
         "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}",
         "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}",
-        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{018D5C66-4533-4307-9B53-224DE2ED1FE6}",
-        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StorageProvider\OneDrive",
-        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StorageProviderStatus\OneDrive"
+        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
     ) | ForEach-Object { Remove-Item -Path $_ -Recurse -Force }
-
-    # RunNotification temizliği
-    $runNotificationPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunNotification"
-    if (Test-Path $runNotificationPath) {
-        Get-Item -Path $runNotificationPath | 
-        Select-Object -ExpandProperty Property | 
-        Where-Object { $_ -like "StartupTNotiOneDrive*" -or $_ -like "StartupTNotiMicrosoftEdgeAutoLaunch*" } | 
-        ForEach-Object { Remove-ItemProperty -Path $runNotificationPath -Name $_ -Force }
-    }
-
-    # Compatibility Assistant Store temizliği
-    $compatStorePath = "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store"
-    if (Test-Path $compatStorePath) {
-        Get-Item -Path $compatStorePath |
-        Select-Object -ExpandProperty Property |
-        Where-Object { $_ -like "*OneDrive*" } |
-        ForEach-Object { Remove-ItemProperty -Path $compatStorePath -Name $_ -Force }
-    }
-
-    # Winlogon ExcludeProfileDirs temizliği
-    $winlogonPath = "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
-    if (Test-Path $winlogonPath) {
-        Remove-ItemProperty -Path $winlogonPath -Name "ExcludeProfileDirs" -Force -ErrorAction SilentlyContinue
-    }
-
-    # RegisteredApplications OneDrive temizliği
-    $regAppsPath = "HKCU:\Software\RegisteredApplications"
-    if (Test-Path $regAppsPath) {
-        Remove-ItemProperty -Path $regAppsPath -Name "OneDrive" -Force -ErrorAction SilentlyContinue
-    }
-
-    # Shell Folders edit
-    $username = $env:USERNAME
-    $shellFoldersPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
-    
-    # Edit folders
-    Set-ItemProperty -Path $shellFoldersPath -Name "My Music" -Value "C:\Users\$username\Music" -Type String -Force
-    Set-ItemProperty -Path $shellFoldersPath -Name "My Pictures" -Value "C:\Users\$username\Pictures" -Type String -Force
-    Set-ItemProperty -Path $shellFoldersPath -Name "My Video" -Value "C:\Users\$username\Videos" -Type String -Force
-    Set-ItemProperty -Path $shellFoldersPath -Name "Personal" -Value "C:\Users\$username\Documents" -Type String -Force
-    Set-ItemProperty -Path $shellFoldersPath -Name "Desktop" -Value "C:\Users\$username\Desktop" -Type String -Force
-    Set-ItemProperty -Path $shellFoldersPath -Name "Favorites" -Value "C:\Users\$username\Favorites" -Type String -Force
 
     # Restart Explorer
     Get-Process explorer | Stop-Process -Force
